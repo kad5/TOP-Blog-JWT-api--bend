d3ec34556e5c1a3920868ddd06e767d5b6b8be1d
kad5 <khaled.mohamed.1807@gmail.com>
2025-02-18 20:33:28 +0200

    all queries done

---
+++ queries/articles.js
@@ -0,0 +1,176 @@
+const prisma = require("../config/prisma");
+
+const create = {
+  article: async (title, body, authorId, isPremium, isPublished, category) => {
+    try {
+      return await prisma.article.create({
+        data: { title, body, authorId, isPremium, isPublished, category },
+      });
+    } catch (error) {
+      throw new Error("failed to create article");
+    }
+  },
+  comment: async (content, userId, articleId) => {
+    try {
+      return await prisma.comment.create({
+        data: { content, userId, articleId },
+      });
+    } catch (error) {
+      throw new Error("failed to create comment");
+    }
+  },
+  favorite: async (userId, articleId) => {
+    try {
+      return await prisma.favorite.create({
+        data: { userId, articleId },
+      });
+    } catch (error) {
+      throw new Error("failed to add to favorites");
+    }
+  },
+};
+
+const get = {
+  articleById: async (id) => {
+    try {
+      return await prisma.article.findUnique({
+        where: { id },
+        include: {
+          author: { select: { id: true, username: true, createdAt: true } },
+          _count: { select: { favorites: true } },
+        },
+      });
+    } catch (error) {
+      throw new Error("failed to find the requested article");
+    }
+  },
+  commentsByArticleId: async (articleId) => {
+    try {
+      return await prisma.comment.findMany({
+        where: { articleId },
+        include: {
+          user: { select: { id: true, username: true, createdAt: true } },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      throw new Error("failed to fetch the comments for this article");
+    }
+  },
+  allArticlesList: async (
+    page = 1,
+    pageSize = 10,
+    sortDirection = "desc",
+    freeOnly = false
+  ) => {
+    page = Math.max(1, page); // to make sure page is at least 1
+    pageSize = Math.max(1, pageSize); // to make sure pageSize is at least 1 article
+    const skip = (page - 1) * pageSize;
+    const orderBy = { createdAt: sortDirection };
+    const where = freeOnly === true ? { isPremium: false } : {};
+
+    try {
+      const articles = await prisma.article.findMany({
+        where,
+        skip,
+        take: pageSize,
+        orderBy,
+        select: {
+          id: true,
+          title: true,
+          isPremium: true,
+          isPublished: true,
+          category: true,
+          createdAt: true,
+          updatedAt: true,
+          author: { select: { id: true, username: true, createdAt: true } },
+          _count: { select: { favorites: true, comments: true } },
+        },
+      });
+      return articles;
+    } catch (error) {
+      throw new Error("Failed to fetch the articles list.");
+    }
+  },
+};
+
+const update = {
+  article: async (id, data, userId) => {
+    try {
+      const article = await prisma.article.findUnique({
+        where: { id },
+        select: { authorId: true, title: true },
+      });
+      if (!article) throw new Error("The article does not exist");
+      if (article.authorId !== userId)
+        throw new Error("you are not authorized to modify this article");
+
+      return await prisma.article.update({ where: { id }, data });
+    } catch (error) {
+      throw new Error("Failed to update the acticle");
+    }
+  },
+  comment: async (id, data) => {
+    try {
+      const comment = await prisma.comment.findUnique({
+        where: { id },
+        select: { userId: true },
+      });
+
+      if (!comment) throw new Error("The comment does not exist");
+      if (comment.userId !== userId)
+        throw new Error("you are not authorized to delete this comment");
+
+      return await prisma.comment.update({ where: { id }, data });
+    } catch (error) {
+      throw new Error("Failed to update the comment");
+    }
+  },
+};
+const dlt = {
+  article: async (id, userId) => {
+    try {
+      const article = await prisma.article.findUnique({
+        where: { id },
+        select: { authorId: true, title: true },
+      });
+
+      if (!article) throw new Error("The article does not exist");
+      if (article.authorId !== userId)
+        throw new Error("you are not authorized to delete this article");
+
+      await prisma.article.delete({ where: { id } });
+      return { message: `Article: ${article.title} deleted successfully` };
+    } catch (error) {
+      throw new Error("Failed to delete the acticle");
+    }
+  },
+  comment: async (id, userId) => {
+    try {
+      const comment = await prisma.comment.findUnique({
+        where: { id },
+        select: { userId: true },
+      });
+
+      if (!comment) throw new Error("The comment does not exist");
+      if (comment.userId !== userId)
+        throw new Error("you are not authorized to delete this comment");
+
+      await prisma.comment.delete({ where: { id } });
+      return { message: "Comment deleted successfully" };
+    } catch (error) {
+      throw new Error("Failed to delete this comment");
+    }
+  },
+  favorite: async (id) => {
+    try {
+      return await prisma.favorite.delete({ where: { id } });
+    } catch (error) {
+      throw new Error("Failed to unlike the article");
+    }
+  },
+};
+
+module.exports = { create, get, update, dlt };
---
+++ queries/users.js
@@ -0,0 +1,197 @@
+const prisma = require("../config/prisma");
+
+const create = {
+  user: async (username, password, role) => {
+    try {
+      return await prisma.user.create({
+        data: { username, password, role },
+      });
+    } catch (error) {
+      throw new Error("failed to create user");
+    }
+  },
+  loginAttempt: async (userId, ip) => {
+    try {
+      await prisma.$transaction([
+        // Update last login
+        prisma.lastLogin.upsert({
+          where: { userId },
+          create: { userId, ip },
+          update: { loginTime: new Date(), ip },
+        }),
+        // Create login history entry
+        prisma.loginHistory.create({
+          data: { userId, ip },
+        }),
+      ]);
+    } catch (error) {
+      throw new Error(
+        `Failed to record login for user ${userId}: ${error.message}`
+      );
+    }
+  },
+};
+
+const get = {
+  userById: async (id) => {
+    // this is used on each time the jwt is checked to check acess level & last login date (single device policy)
+    try {
+      return await prisma.user.findUnique({
+        where: { id },
+        include: { lastLogin: true },
+      });
+    } catch (error) {
+      throw new Error("failed to find this user id");
+    }
+  },
+  userByUsername: async (username) => {
+    // this is only used when someone is logging in, so here we dont need to lastlogin as we are making a new token
+    try {
+      return await prisma.user.findUnique({ where: { username } });
+    } catch (error) {
+      throw new Error("failed to find this username");
+    }
+  },
+  userProfile: async (id) => {
+    //review the code comments at the end of this page
+    try {
+      return await prisma.user.findUnique({
+        where: { id },
+        select: {
+          username: true,
+          motto: true,
+          role: true,
+          isPaying: true,
+          createdAt: true,
+          lastLogin: {
+            select: {
+              loginTime: true,
+            },
+          },
+          articles: {
+            select: {
+              id: true,
+              title: true,
+              category: true,
+              isPremium: true,
+              isPublished: true,
+              createdAt: true,
+              updatedAt: true,
+              _count: {
+                select: { favorites: true, comments: true },
+              },
+            },
+          },
+          favorites: {
+            select: {
+              article: {
+                select: {
+                  id: true,
+                  title: true,
+                  category: true,
+                  isPremium: true,
+                  isPublished: true,
+                  createdAt: true,
+                  updatedAt: true,
+                  author: {
+                    username: true,
+                    id: true,
+                  },
+                  _count: {
+                    select: { favorites: true, comments: true },
+                  },
+                },
+              },
+            },
+          },
+          comments: {
+            select: {
+              id: true,
+              content: true,
+              createdAt: true,
+              article: {
+                select: { id: true, title: true },
+              },
+            },
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error("failed to fetch the user profile data");
+    }
+  },
+  allUsersList: async () => {
+    try {
+      return await prisma.user.findMany({
+        select: {
+          id: true,
+          username: true,
+          role: true,
+        },
+        orderBy: { role: "desc" },
+      });
+    } catch (error) {
+      throw new Error("failed to fetch users list");
+    }
+  },
+  adminAllUsers: async () => {
+    try {
+      return await prisma.user.findMany({
+        include: { lastLogin: true },
+        orderBy: { role: "desc" },
+      });
+    } catch (error) {
+      throw new Error("failed to fetch users list");
+    }
+  },
+  adminAllLogins: async () => {
+    try {
+      return await prisma.loginHistory.findMany({
+        orderBy: { loginTime: "desc" },
+      });
+    } catch (error) {
+      throw new Error("failed to fetch users list");
+    }
+  },
+};
+
+const update = {
+  user: async (id, data) => {
+    try {
+      return await prisma.user.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      throw new Error("Failed to update user");
+    }
+  },
+};
+
+const dlt = {
+  user: async (id) => {
+    try {
+      return await prisma.user.delete({ where: { id } });
+    } catch (error) {
+      throw new Error("Failed to delete the user");
+    }
+  },
+};
+
+module.exports = { create, get, update, dlt };
+
+/*
+    the  very long query to fetch a user profile can actually be separated into 4 smaller queries (user details, articles by user, user favorites, user comments) however since the dataset will be small , no need to have 4 separate fetch requests compared to just one with data consistency. I think this could perform well and be maintained till the tables are of severeal thousands entries, then separating the requests and including pagination would be the best appraoch
+
+      sample routes for the user profile tabs
+          GET /api/users/:id            // Basic info
+          GET /api/users/:id/articles   // Tab 1
+          GET /api/users/:id/favorites  // Tab 2
+          GET /api/users/:id/comments   // Tab 3
+      pagination
+        const userArticles = await prisma.article.findMany({
+        where: { authorId: id },
+          take: 10,
+          skip: page * 10,
+      });
+  */
